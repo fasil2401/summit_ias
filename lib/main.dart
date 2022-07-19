b7e7a64a66fa9b49bb61e38157ae69a3348a83f0
fasil2401 <fasil2401@gmail.com>
2022-07-19 12:42:03 +0530

    increment counter

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment Counter',
         child: const Icon(Icons.add),
       ), 
     );
