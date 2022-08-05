7aee05ff55bd7f4b2b7af045782d183d7431e218
thisisrishal <mrishal.ap@gmail.com>
2022-08-05 12:02:51 +0530

    first commit by rishal

---
+++ lib/view/phone_number/phone_number.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PhoneNumber extends StatelessWidget {
+  const PhoneNumber({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Continue with phone'),
+
+        ),
+        body: ListView(
+          children: const [
+            
+
+          ],
+        ));
+  }
+}
---
+++ lib/view/splash_screen/splash_screen_two.dart
@@ -0,0 +1,70 @@
+import 'package:action_slider/action_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class SplashScreenTwo extends StatefulWidget {
+  const SplashScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreenTwo> createState() => _SplashScreenTwoState();
+}
+
+class _SplashScreenTwoState extends State<SplashScreenTwo> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: height * 0.95,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icons/bg 1.png',
+                  ),
+                  fit: BoxFit.cover),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(-.5, -.7),
+            child: SizedBox(
+              height: 20.h,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Start Learning with ',
+                    style: const TextStyle()
+                        .copyWith(fontSize: 22.sp, color: Colors.white),
+                  ),
+                  Text(
+                    'SUMMIT IAS ',
+                    style: const TextStyle()
+                        .copyWith(fontSize: 22.sp, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0, 0.97),
+            child: ActionSlider.standard(
+              height: 15.w,
+              width: 90.w,
+              reverseSlideAnimationCurve: Curves.easeIn,
+              child: const Text('Slide to Learn'),
+            ),
+          )
+        ],
+      ),
+    );
+
+    // child: Image.asset('assets/icons/bg 1.png', fit: BoxFit.cover));
+  }
+}
