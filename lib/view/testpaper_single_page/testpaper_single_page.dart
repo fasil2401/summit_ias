1b52d9e80c54ac174000310dd429c5500f23894b
mymxxn <achujaleel123@gmail.com>
2022-08-06 22:09:55 +0530

    test paper single page

--- lib/view/testpaper_single_page/testpaper_single_page.dart
+++ lib/view/testpaper_single_page/testpaper_single_page.dart
@@ -71,17 +71,265 @@ class TestPaperSinglePage extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
             Container(
-              height: 250,
+              height: 200,
               width: double.infinity,
               decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(19),
                   image: DecorationImage(
                       fit: BoxFit.fill,
-                      image: AssetImage("assets/icons/Rectangle 12.jpg"))),
-            )
+                      image: AssetImage("assets/icons/Rectangle 1.jpg"))),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("IAS examination sample",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              Text(
+                                "5 questions | 10 minutes",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(124, 60, 229, 100),
+                                  Color.fromRGBO(60, 161, 229, 100),
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Start",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("IAS examination sample",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              Text(
+                                "5 questions | 10 minutes",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(124, 60, 229, 100),
+                                  Color.fromRGBO(60, 161, 229, 100),
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Start",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("IAS examination sample",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              Text(
+                                "5 questions | 10 minutes",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(124, 60, 229, 100),
+                                  Color.fromRGBO(60, 161, 229, 100),
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Start",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("IAS examination sample",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              Text(
+                                "5 questions | 10 minutes",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(124, 60, 229, 100),
+                                  Color.fromRGBO(60, 161, 229, 100),
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Start",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 22),
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("IAS examination sample",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18)),
+                              Text(
+                                "5 questions | 10 minutes",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 30,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(124, 60, 229, 100),
+                                  Color.fromRGBO(60, 161, 229, 100),
+                                ])),
+                            child: Center(
+                              child: Text(
+                                "Start",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
